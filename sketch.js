5ffbbb2d4507c8ab1bf68724823ab6840cc5c674
Ayline Marín Oliva <aylinemarinoliva@gmail.com>
2025-08-11 17:10:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,14 +3,12 @@ function setup() {
   background(168, 0, 255);
 }
 
-
 function draw() {
   d = random(10, 60);
   colorMode(HSB, 260, 100, 100);
   fill(random(0,360)100, 100);
 ellipse(mouseX, mouseY, d, d);
 }
-
 
 function keyPressed() {
   if (key === 'p') {
